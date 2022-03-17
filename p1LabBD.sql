cfdb1b54e72909724e628479ec4be8bc89ce40e1
Gustavo Gonçalves Cavichioli <gustavog.cavichioli@gmail.com>
2022-03-17 10:49:52 -0300

    finaliza função organizar grupos P1

--- p1LabBD.sql
+++ p1LabBD.sql
@@ -49,14 +49,38 @@ INSERT INTO times VALUES('Corinthians', 'São Paulo', 'Neo Química Arena', 1),
 						('São Bento', 'Sorocaba', 'Walter Ribeiro', 0),
 						('São Caetano', 'São Caetano do Sul', 'Anacletto Campanella', 0)
 
-CREATE PROCEDURE sp_gerador_grupos(@saida INT) 
+CREATE PROCEDURE sp_gerador_grupos 
 AS
-	DECLARE @loopGrupo INT, @loopTime INT, @time INT, @grupo INT
-	SET @loopGrupo = 0 SET @loopTime = 0
-	WHILE (@loopTime < 16) BEGIN
-		SET @time = CAST((RAND() * 15 + 1) AS INT)
-		SET @grupo = CAST((RAND() * 3 + 1) AS INT)
+	DECLARE @loopGrupo INT, @grupo INT
+	SET @loopGrupo = 1
+
+	/*Loop dos grupos*/
+	WHILE (@loopGrupo < 5) BEGIN
+		/*SET @grupo = CAST((RAND() * 3 + 1) AS INT)*/
+		DECLARE @grupoValido BIT, @nomeGrupo VARCHAR(1)
+		EXEC sp_valida_grupo @loopGrupo, @grupoValido OUTPUT, @nomeGrupo OUTPUT
+		IF(@grupoValido = 1) BEGIN
+			
+			/*Loop dos times*/
+			DECLARE @loopTime INT SET @loopTime = 1
+			WHILE (@loopTime < 5) BEGIN
+
+				DECLARE @timeValido BIT, @time INT
+				SET @time = CAST((RAND() * 16 + 1) AS INT)
+
+				/*Validação dos times aleatórios*/
+				EXEC sp_valida_times @time, @nomeGrupo, @timeValido OUTPUT
+				IF(@timeValido = 1)BEGIN
+					DECLARE @query VARCHAR(100)
+					SET @query = 'INSERT INTO grupos VALUES ('''+@nomeGrupo+''', '+CAST(@time AS VARCHAR(2))+')'
+					EXEC(@query)
+					SET @loopTime = @loopTime + 1
+				END
+			END
+			SET @loopGrupo = @loopGrupo + 1
+		END
 	END
+	SELECT * FROM grupos
 
 CREATE PROCEDURE sp_valida_grupo(@cod INT, @valido BIT OUTPUT, @grupo VARCHAR(1) OUTPUT)
 AS
@@ -85,11 +109,6 @@ AS
 		SET @valido = 1
 	END
 
-DECLARE @valida BIT, @grops VARCHAR(1)
-EXEC sp_valida_grupo 5, @valida OUTPUT, @grops OUTPUT
-PRINT @valida
-PRINT @grops
-
 CREATE PROCEDURE sp_valida_times(@cod INT, @grupo VARCHAR(1), @valido BIT OUTPUT)
 AS
 	DECLARE @participando INT
@@ -101,27 +120,36 @@ AS
 
 		SELECT @unico_time = fl_unico_time FROM times WHERE CodigoTime = @cod
 
-		/*Identifica se já tem um time "especial" no grupo*/
-		IF(@unico_time = 1)BEGIN
-
-			SELECT @unico_no_grupo = g.CodigoTime FROM grupos AS g 
+		SELECT @unico_no_grupo = g.CodigoTime FROM grupos AS g 
 			INNER JOIN times as t ON g.CodigoTime = t.CodigoTime
 			WHERE t.fl_unico_time = 1 AND g.Grupo LIKE @grupo
 
-			IF(@unico_no_grupo IS NULL)BEGIN
+		/*Identifica se já tem um time "especial" no grupo*/
+		IF(@unico_no_grupo IS NULL)BEGIN
+
+			IF(@unico_time = 1)BEGIN
 				SET @valido = 1
+			END 
+			ELSE BEGIN
+				SET @valido = 0
 			END
 		END
 		ELSE BEGIN
-			SET @valido = 1
+			IF(@unico_time = 0)BEGIN
+				SET @valido = 1
+			END
+			ELSE BEGIN
+				SET @valido = 0
+			END
 		END
 
 	END
+	ELSE BEGIN
+		SET @valido = 0
+	END
 
 
-SELECT * FROM grupos
 
-USE master
 
 SELECT * FROM times
 SELECT * FROM grupos
@@ -133,34 +161,3 @@ DELETE FROM grupos
 DROP TABLE grupos
 DELETE FROM jogos
 DROP TABLE jogos
-
-DECLARE @participand INT, @bit BIT
-SELECT @participand = CodigoTime FROM grupos WHERE CodigoTime = 3
-IF(@participand IS NOT NULL) BEGIN
-	SET @bit = 1
-END
-ELSE BEGIN
-	SET @bit = 0
-END
-
-PRINT @participand
-PRINT @bit
-
-INSERT INTO grupos VALUES ('A', 1)
-INSERT INTO grupos VALUES ('A', 2)
-INSERT INTO grupos VALUES ('A', 12)
-SELECT CodigoTime FROM grupos WHERE CodigoTime = 2
-
-DECLARE @unico_no_grupo INT, @bit BIT
-SELECT @unico_no_grupo = g.CodigoTime FROM grupos AS g 
-		INNER JOIN times as t 
-		ON g.CodigoTime = t.CodigoTime
-		WHERE t.fl_unico_time = 1 AND g.Grupo LIKE 'A'
-PRINT @unico_no_grupo
-IF(@unico_no_grupo IS NULL) BEGIN
-	SET @bit = 1
-END
-ELSE BEGIN
-	SET @bit = 0
-END
-PRINT @bit
